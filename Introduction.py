5a224e10425c64dcfcae298a9b0bc44b3318828c
Nigel34 <ichenihinigel@gmail.com>
2025-02-03 13:15:37 +0300

    explainable

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+#print is a method used to display a value
+
+print("Frankenstine")
+print("eMobilis")
+print(39)
+print(100*2)
---
+++ Variable.py
@@ -0,0 +1,18 @@
+#Variable is a container used to store a value
+
+number = 10
+firstname = "Nigel"
+lastname = "Unknown"
+language = "Python"
+b = 15
+a = 10
+c = 25
+print(number)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(language, "is a web programming language")
+print(a+b)
+#the sum of 10 and 15 is 25
+
+print("the sum of", a ,"and", b ,"is", c)
